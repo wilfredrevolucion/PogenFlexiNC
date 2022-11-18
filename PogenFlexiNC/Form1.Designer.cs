03f1728cb564e073f56586107f84322e45ac5f8f
Wilfred <wilfredrevolucion@gmail.com>
2022-11-17 18:50:25 -0600

    Primer commit

---
+++ PogenFlexiNC/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace PogenFlexiNC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.fecha1 = new System.Windows.Forms.DateTimePicker();
+            this.fecha2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtServidor = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtDirFtp = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtDirLocal = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtArchivo = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(793, 113);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // fecha1
+            // 
+            this.fecha1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.fecha1.Location = new System.Drawing.Point(83, 12);
+            this.fecha1.Name = "fecha1";
+            this.fecha1.Size = new System.Drawing.Size(114, 20);
+            this.fecha1.TabIndex = 1;
+            // 
+            // fecha2
+            // 
+            this.fecha2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.fecha2.Location = new System.Drawing.Point(274, 12);
+            this.fecha2.Name = "fecha2";
+            this.fecha2.Size = new System.Drawing.Size(114, 20);
+            this.fecha2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Fecha inicio:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(214, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Fecha fin:";
+            // 
+            // txtServidor
+            // 
+            this.txtServidor.Location = new System.Drawing.Point(108, 159);
+            this.txtServidor.Name = "txtServidor";
+            this.txtServidor.ReadOnly = true;
+            this.txtServidor.Size = new System.Drawing.Size(100, 20);
+            this.txtServidor.TabIndex = 7;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(108, 185);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.ReadOnly = true;
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Directorio FTP:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(108, 211);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.ReadOnly = true;
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Servidor FTP:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 188);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Usuario FTP:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 214);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Contraseña FTP:";
+            // 
+            // txtDirFtp
+            // 
+            this.txtDirFtp.Location = new System.Drawing.Point(108, 237);
+            this.txtDirFtp.Name = "txtDirFtp";
+            this.txtDirFtp.ReadOnly = true;
+            this.txtDirFtp.Size = new System.Drawing.Size(280, 20);
+            this.txtDirFtp.TabIndex = 18;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(547, 265);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(261, 18);
+            this.progressBar1.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(402, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Estado:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(405, 157);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(403, 96);
+            this.richTextBox1.TabIndex = 22;
+            this.richTextBox1.Text = "";
+            // 
+            // txtDirLocal
+            // 
+            this.txtDirLocal.Location = new System.Drawing.Point(108, 263);
+            this.txtDirLocal.Name = "txtDirLocal";
+            this.txtDirLocal.ReadOnly = true;
+            this.txtDirLocal.Size = new System.Drawing.Size(280, 20);
+            this.txtDirLocal.TabIndex = 23;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 266);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(80, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Directorio lcoal:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(222, 162);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Archivo:";
+            // 
+            // txtArchivo
+            // 
+            this.txtArchivo.Location = new System.Drawing.Point(288, 159);
+            this.txtArchivo.Name = "txtArchivo";
+            this.txtArchivo.ReadOnly = true;
+            this.txtArchivo.Size = new System.Drawing.Size(100, 20);
+            this.txtArchivo.TabIndex = 26;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 4000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 4000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 5000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 299);
+            this.Controls.Add(this.txtArchivo);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtDirLocal);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txtDirFtp);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtServidor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fecha2);
+            this.Controls.Add(this.fecha1);
+            this.Controls.Add(this.dataGridView1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker fecha1;
+        private System.Windows.Forms.DateTimePicker fecha2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtServidor;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtDirFtp;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtDirLocal;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtArchivo;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+    }
+}
+
---
+++ PogenFlexiNC/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using ClosedXML.Excel;
+using System.Net;
+using System.IO;
+using SpreadsheetLight;
+
+namespace PogenFlexiNC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conexionbd = new SqlConnection("Data Source = .; Initial Catalog = DIREKTOR_DATA; Integrated Security = True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label3.Text = "Iniciando...";
+            progressBar1.Value = 15; 
+
+            DateTime diaAnterior = DateTime.Today.AddDays(-5);
+            fecha1.Text = diaAnterior.ToString();
+            DateTime fechaactual = DateTime.Today;
+            fecha2.Text = fechaactual.ToString();
+
+            conexionbd.Open();
+            string queryftp = "select srv_nom, ftp_usr, ftp_pas, ftp_dir_fac, local_dir, nom_arch from inv_cad where substring(cod_emp, 1, 3) = '001' ";
+            SqlCommand comando = new SqlCommand(queryftp, conexionbd);
+            SqlDataReader leedato = comando.ExecuteReader();
+            leedato.Read();
+
+            string servidorftp = leedato.GetString(0);
+            txtServidor.Text = servidorftp;
+            string usuarioftp = leedato.GetString(1);
+            txtUsuario.Text = usuarioftp;
+            string contrasenaftp = leedato.GetString(2);
+            txtPassword.Text = contrasenaftp;
+            string directorioftp = leedato.GetString(3);
+            txtDirFtp.Text = directorioftp;
+            string directoriolocal = leedato.GetString(4);
+            txtDirLocal.Text = directoriolocal;
+            string nombrearchivo = leedato.GetString(5);
+            txtArchivo.Text = nombrearchivo;
+
+            conexionbd.Close();
+
+            timer1.Start();
+        }
+
+        private void crearArchivo(Stream origen, Stream destino)
+        {
+            //este permite crear el archivo en el FTP
+            byte[] buffer = new byte[1024];
+            int bytesLeidos = origen.Read (buffer, 0, buffer.Length);
+            while (bytesLeidos != 0)
+            {
+                destino .Write (buffer, 0, bytesLeidos);
+                bytesLeidos = origen.Read (buffer, 0, buffer.Length);    
+            }
+            origen.Close ();
+            destino.Close ();
+        }
+
+        private string GetNameFile()
+        {
+            string archivolog = "";
+            archivolog = "log_"+DateTime .Now.Day + "_"+DateTime.Now.Month+"_"+DateTime.Now.Year;
+            return archivolog;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //este proceso hace el query a la base de datos para generar el reporte para el archivo
+            label3.Text = "Consultando...";
+            progressBar1 .Value = 35;
+
+            string f1 = fecha1.Text;
+            string f2 = fecha2.Text;
+
+            f1 = fecha1.Value.ToString("yyyyMMdd");
+            f2 = fecha2.Value.ToString("yyyyMMdd");
+
+            string querydatos = "(SELECT (SELECT TOP 1 cod_flx FROM inv_cad WHERE cod_emp = '001') as TID, (SELECT TOP 1 nom_tda FROM inv_cad WHERE cod_emp = '001') as TIENDA,CONVERT(varchar, fac_enc_fac.fyh_nrd, 120) as FECHA,COUNT(DISTINCT(fac_enc_fac.num_doc)) as TICKETS,SUM(fac_enc_fac.mto) as VENTAS,(SELECT SUM(fac_det_fac.ctd) FROM fac_det_fac  WHERE fac_det_fac.id_doc = fac_enc_fac.id_doc)  as PIEZAS,10 as HORAS FROM fac_enc_fac WHERE cod_emp = '001' AND fac_enc_fac.mto > 0 AND fac_enc_fac.fec BETWEEN ' " + f1 + " ' and ' " + f2 + " ' GROUP BY fac_enc_fac.num_doc, fac_enc_fac.fyh_nrd, fac_enc_fac.id_doc) UNION ( SELECT (SELECT TOP 1 cod_flx FROM inv_cad WHERE cod_emp = '001') as TID, (SELECT TOP 1 nom_tda FROM inv_cad WHERE cod_emp = '001') as TIENDA, CONVERT(varchar, inv_encingbod.fyh_nrd, 120) as FECHA, COUNT(DISTINCT(inv_encingbod.num_doc)) as TICKETS, SUM(inv_encingbod.mto_tot_nc) as VENTAS, (SELECT SUM(inv_detingbod.ctd) FROM inv_detingbod  WHERE  inv_detingbod.id_doc = inv_encingbod.id_doc) as PIEZAS, 10 as HORAS FROM inv_encingbod WHERE cod_emp = '001' AND SUBSTRING(inv_encingbod.id_doc,10,3) = 'N1D' AND inv_encingbod.fec BETWEEN ' " + f1 + " ' and ' " + f2 + " ' GROUP BY inv_encingbod.num_doc, inv_encingbod.fyh_nrd, inv_encingbod.id_doc ) ";
+            SqlDataAdapter adaptador = new SqlDataAdapter(querydatos, conexionbd);
+            DataTable tabladatos = new DataTable();
+            adaptador.Fill(tabladatos);
+
+            dataGridView1.DataSource = tabladatos;
+
+            timer1.Stop();
+            timer2.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //este proceso crea el archivo excel en directorio local
+            label3.Text = "Creando archivo...";
+            progressBar1.Value = 75;
+
+            SLDocument documento = new SLDocument();
+
+            documento.ImportDataTable(1, 1, (DataTable)dataGridView1.DataSource, true);
+            documento.SaveAs(txtDirLocal.Text + txtArchivo.Text);
+            label3.Text = "Archivo creado...";
+
+            timer2.Stop();
+            timer3.Start();
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            label3.Text = "Conectando FTP...";
+            progressBar1.Value = 85;
+
+            try
+            {
+                //conectar con servidor FTP
+                Uri miuri = new Uri(txtServidor.Text + txtDirFtp.Text);
+                FtpWebRequest clienteRequest = (FtpWebRequest)WebRequest.Create(miuri);
+                NetworkCredential credenciales = new NetworkCredential();
+
+                credenciales.UserName = txtUsuario.Text;
+                credenciales.Password = txtPassword.Text;
+
+                clienteRequest.Credentials = credenciales;
+                clienteRequest.EnableSsl = false;
+                clienteRequest.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+                clienteRequest.KeepAlive = true;
+                clienteRequest.UsePassive = true;
+
+                FtpWebResponse respuesta = (FtpWebResponse)clienteRequest.GetResponse();
+
+                StreamReader sr = new StreamReader(respuesta.GetResponseStream(), Encoding.UTF8);
+                string resultado = sr.ReadToEnd();
+                richTextBox1.Text = respuesta.WelcomeMessage + "Conectado a FTP..." + "\r\n" + resultado;
+                respuesta.Close();
+
+                timer3.Stop();
+                timer4.Start();
+            }  
+            catch (Exception ex) 
+             {
+                richTextBox1.Text = ex.Message ;
+
+                string archivolog = GetNameFile();
+                string cadena = "";
+                cadena = DateTime .Now +"-"+ex.Message+Environment.NewLine;
+                StreamWriter sw = new StreamWriter(@"C:\archivospogen\Errores\"+"/"+archivolog+".txt",true);
+                sw.Write (cadena);
+                sw.Close();
+
+                timer3 .Stop ();
+                timer4.Stop();
+                this.Close ();
+
+             }
+
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            label3.Text = "Subiendo archivo...";
+            progressBar1 .Value = 95;
+
+
+            try
+            {
+                //sube archivo a FTP
+                //aca se indica el directorio del FTP donde se subirá el archivo
+                Uri uri = new Uri(txtServidor.Text + txtDirFtp.Text + txtArchivo.Text);
+                FtpWebRequest clienteRequest = (FtpWebRequest)WebRequest.Create(uri);
+                NetworkCredential credenciales = new NetworkCredential();
+
+                credenciales.UserName = txtUsuario.Text;
+                credenciales.Password = txtPassword.Text;
+
+                clienteRequest.Credentials = credenciales;
+                clienteRequest.EnableSsl = false;
+                clienteRequest.Method = WebRequestMethods.Ftp.UploadFile;
+                clienteRequest.KeepAlive = true;
+                clienteRequest.UsePassive = true;
+
+                Stream destino = clienteRequest.GetRequestStream();
+                //aca se indica el directorio del archivo a enviar al FTP
+                FileStream origen = new FileStream(txtDirLocal.Text + txtArchivo.Text, FileMode.Open, FileAccess.Read);
+                crearArchivo(origen, destino);
+
+                progressBar1.Value = 100;
+                label3.Text = "Fin proceso";
+
+                timer4.Stop();
+                this.Close();
+            } 
+            catch (Exception ex)
+            {
+                richTextBox1.Text = ex.Message;
+
+                string archivolog = GetNameFile();
+                string cadena = "";
+                cadena = DateTime.Now + "-" + ex.Message + Environment.NewLine;
+                StreamWriter sw = new StreamWriter(@"C:\archivospogen\Errores\" + "/" + archivolog + ".txt", true);
+                sw.Write(cadena);
+                sw.Close();
+
+                timer4.Stop();
+                this.Close();
+            }
+
+        }
+
+
+    }
+}
